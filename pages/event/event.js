0f5dbbfbc50ee88a356e10b6ca6a5efcd4b49605
wanghao8080 <117663444@qq.com>
2017-08-02 11:47:43 +0800

    Page：页面的生命周期

--- pages/event/event.js
+++ pages/event/event.js
@@ -1,1 +1,17 @@
-Page({})
+Page({
+  onLoad() {
+    console.log('页面加载')
+  },
+  onShow() {
+    console.log('页面显示')
+  },
+  onReady() {
+    console.log('页面首次渲染完成')
+  },
+  onHide() {
+    console.log('页面隐藏')
+  },
+  onUnload() {
+    console.log('页面卸载')
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,5 +3,20 @@ const app = getApp()
 Page({
   data: {
     greeting: app.globalData.greeting
+  },
+  onLoad() {
+    console.log('页面加载')
+  },
+  onShow() {
+    console.log('页面显示')
+  },
+  onReady() {
+    console.log('页面首次渲染完成')
+  },
+  onHide() {
+    console.log('页面隐藏')
+  },
+  onUnload() {
+    console.log('页面卸载')
   }
 })
