0965f9124bfa81b854751fcdee0fee432c144fac
wanghao8080 <117663444@qq.com>
2017-08-02 11:36:05 +0800

    App：小程序的生命周期函数

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-App({})
+App({
+  onLaunch(options) {
+    console.log('初始化')
+    console.log(options)
+  },
+  onShow(options) {
+    console.log('显示')
+    console.log(options)
+  },
+  onHide() {
+    console.log('隐藏')
+  },
+  onError(error) {
+    console.log(error)
+  }
+})
