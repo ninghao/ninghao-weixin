c85373f7582c828b330bfb7a1a52626a4fd019d2
wanghao8080 <117663444@qq.com>
2017-08-31 12:46:00 +0800

    发送请求：wx.request

--- pages/api/api.js
+++ pages/api/api.js
@@ -3,4 +3,13 @@ Page({
   data: {
 
   },
+
+  tapHandler(event) {
+    wx.request({
+      url: 'https://sandbox.ninghao.net',
+      success: (response) => {
+        console.log(response)
+      }
+    })
+  }
 })
