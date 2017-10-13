c1d380f1e94c625ecc696865173a0280fa5cff67
wanghao8080 <117663444@qq.com>
2017-10-13 14:30:18 +0800

    数据缓存：设置与获取数据

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,47 +1,22 @@
 // pages/api/api.js
 Page({
   data: {
-    images: [],
-    progress: []
   },
 
   tapHandler(event) {
-    wx.chooseImage({
-      count: 9,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success: (response) => {
-        console.log(response)
-        this.setData({
-          images: response.tempFilePaths
-        })
-
-        response.tempFilePaths.map((filePath, index) => {
-          const uploadTask = wx.uploadFile({
-            url: 'https://sandbox.ninghao.net/api/uploads',
-            filePath: filePath,
-            name: 'uploads',
-            success: (response) => {
-              console.log(response)
-            }
-          })
-
-          uploadTask.onProgressUpdate((response) => {
-            this.data.progress[index] = response.progress
-
-            this.setData({
-              progress: this.data.progress
-            })
-          })
-        })
-      }
+    wx.setStorage({
+      key: 'name',
+      data: 'ninghao.net',
+      success: this.hello
     })
   },
 
-  preview(event) {
-    wx.previewImage({
-      current: event.target.dataset.src,
-      urls: this.data.images
+  hello() {
+    wx.getStorage({
+      key: 'name',
+      success(response) {
+        console.log('hello ~ ', response.data)
+      }
     })
   }
 })
