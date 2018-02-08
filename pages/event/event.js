7a9a9947d301f97830d2834952eab5ca2886bb5f
wanghao8080 <117663444@qq.com>
2018-02-08 21:24:15 +0800

    实施小程序微信支付功能

--- pages/event/event.js
+++ pages/event/event.js
@@ -1,1 +1,47 @@
-Page({})
+Page({
+  pay () {
+    /**
+     * 请求登录凭证。
+     */
+    wx.login({
+      success: (loginResponse) => {
+        /**
+         * 请求支付参数。
+         */
+        wx.request({
+          url: 'https://sandbox.ninghao.net/checkout/pay',
+          method: 'POST',
+          data: {
+            code: loginResponse.code
+          },
+          success: (paramsResponse) => {
+            console.log(paramsResponse)
+            const params = paramsResponse.data
+
+            /**
+             * 请求支付。
+             */
+            wx.requestPayment({
+              ...params,
+              success: (paymentResponse) => {
+                if (paymentResponse.errMsg === 'requestPayment:ok') {
+                  wx.showToast({
+                    title: '支付成功',
+                    icon: 'success',
+                    duration: 2000
+                  })
+
+                  setTimeout(() => {
+                    wx.switchTab({
+                      url: '/pages/index/index'
+                    })
+                  }, 2000)
+                }
+              }
+            })
+          }
+        })
+      }
+    })
+  }
+})
